4b7fd0babceb9e63d4155f7161970ce456625057
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2022-03-04 20:53:58 -0600

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id='about'>
+    <h2>About Me</h2>
+    <p>I'm finally getting the hang of JSX...kinda!</p>
+    <img src={image} alt='I made this'></img>
+  </div>;
 }
 
 export default About;
